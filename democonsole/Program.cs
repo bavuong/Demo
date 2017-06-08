420a4869708aef3fe0fc8f7181acf96b4a18a58e
vuongnb2 <vuongnb2@fpt.com.vn>
2017-06-08 14:02:37 +0700

    demo

---
+++ democonsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace democonsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestStrim();
+        }
+
+        private static void TestStrim()
+        {
+            string str = "abcdc acbd";
+            var index=str.LastIndexOf("_");
+            var str1 = str.Substring(index+1, str.Length-index-1);
+        }
+    }
+}
